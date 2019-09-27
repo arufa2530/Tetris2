d452a9d7d5099518ba64ef4897d074da353c870c
arufa2530 <moriken.0325@gmail.com>
2019-09-27 17:01:06 +0900

    スコアを追加

---
+++ TetrisCube/Assets/Mori/SCORE.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SCORE : MonoBehaviour
+{
+    //スコアの表示
+    public Text scoreText;
+
+    
+    //スコア
+    private float Score;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //初期化
+        Initialize();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.text = Score.ToString();
+
+        //ポイント100P追加
+        if(Input.GetKeyDown(KeyCode.S))
+        {
+            Score += 100;
+        }
+
+        //途中でリセットするためのキー
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            Score *= 0;
+        }
+
+        //フレームごとに1追加する処理
+        if (Input.GetKey(KeyCode.N))
+        {
+            Score += 1f;
+        }
+    }
+
+    //初期化
+    private void Initialize()
+    {
+        Score = 0;
+    }
+
+    //ポイントの処理
+    private void Addpoint(int point)
+    {
+        Score = Score + point;
+    }
+
+    
+
+}
