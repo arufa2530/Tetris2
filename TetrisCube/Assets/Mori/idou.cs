5da0e6bc0ea4ddd12203482fa8082cec948df2aa
arufa2530 <moriken.0325@gmail.com>
2019-09-30 14:27:00 +0900

    地面聖域

---
+++ TetrisCube/Assets/Mori/idou.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class idou : MonoBehaviour
+{
+    public float I = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        iDou();
+    }
+
+    private void iDou()
+    {
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            transform.position += new Vector3(0, -1, 0);
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            transform.position += new Vector3(0, 1, 0);
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.position += new Vector3(1, 0, 0);
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.position += new Vector3(-1, 0, 0);
+        }
+    }
+
+}
