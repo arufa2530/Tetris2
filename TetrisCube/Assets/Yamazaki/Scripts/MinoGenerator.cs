048df3e6a659d1fba105a5e9fd3305cba2c8cbe2
aaaa <aa@aaa>
2019-09-30 17:07:48 +0900

    ブロックは生成されるけど落ちてこないです。すいません。

---
+++ TetrisCube/Assets/Yamazaki/Scripts/MinoGenerator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinoGenerator : MonoBehaviour
+{
+    public GameObject[] minos;
+
+    public void SpawnMino()
+    {
+        int minoIndex = Random.Range(0, 6);
+
+        Instantiate(minos[minoIndex],
+            transform.position,
+            Quaternion.identity);
+    }
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        SpawnMino();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+}
